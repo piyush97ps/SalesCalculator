fe08a5fa7aadbedc9cd55f7ebd5b2b365298c58d
Piyush Sharma <piyush97ps@gmail.com>
2019-07-17 12:26:29 +0530

    file input

--- README.md
+++ README.md
@@ -4,84 +4,21 @@ Sales Tax Calculator
 Example:
 
 Enter items or product to calculate Sale Tax: 
+Example 
+create input file 'inputFilename.txt' in base folder 'SaleCalculator' like below text line format:
 
-Example 1 book at 12.49
-
+1 book at 12.49
 1 music CD at 14.99
-
 1 chocolate bar at 0.85
-
 1 imported box of chocolates at 10.00
 
 
-TO continue inserting enter Y else anything: y
-
-Enter items or product to calculate Sale Tax: 
-
-Example 1 book at 12.49
-
-1 music CD at 14.99
-
-1 chocolate bar at 0.85
-
-1 box of imported chocolates at 11.25 
-
-
-TO continue inserting enter Y else anything: y
-
-Enter items or product to calculate Sale Tax: 
-
-Example 1 book at 12.49
-
-1 music CD at 14.99
-
-1 chocolate bar at 0.85
-
-1 bottle of perfume at 18.99
-
-
-
-TO continue inserting enter Y else anything: y
-
-Enter items or product to calculate Sale Tax: 
-
-Example 1 book at 12.49
-
-1 music CD at 14.99
-
-1 chocolate bar at 0.85
-
-1 imported bottle of perfume at 27.99
-
-
-TO continue inserting enter Y else anything: y
-
-Enter items or product to calculate Sale Tax: 
-
-Example 1 book at 12.49
-
-1 music CD at 14.99
-
-1 chocolate bar at 0.85
-
-1 packet of headache pills at 9.75
-
-
-
-TO continue inserting enter Y else anything: 
-n
-
+You will get the output like below in output.txt file and terminal
 
 1 imported box of chocolates: 10.50
-
 1 box of imported chocolates: 11.81
-
 1 bottle of perfume: 20.89
-
 1 imported bottle of perfume: 32.19
-
 1 packet of headache pills: 9.75
-
 Sales Taxes: 7.16
-
 Total: 85.14
--- src/SaleTaxCalculator/Main.java
+++ src/SaleTaxCalculator/Main.java
@@ -1,24 +1,38 @@
 package SaleTaxCalculator;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
+
+    // input file path
+    static String inputFilename = "inputFilename.txt";
+    // output file path
+    static String outputFilename = "output.txt";
+
     static String[] BOOKS = {"book ","books "};
     static String[] FOOD = {"chocolates", "chocolate"};
     static String[] MEDICAL = {"pills", "pill"};
     static String IMPORTED = "imported";
     static Scanner in = new Scanner(System.in);
 
-    public static void main(String arg[]) {
-        ArrayList<String> inputList = getInputData();
+    public static void main(String arg[]) throws IOException {
+        ArrayList<String> inputList = getFileInput(); //getInputData();
         ArrayList<String> outputList = getOutputData(inputList);
+        getFileOutput(outputList);
         for(String output: outputList){
             System.out.println(output);
         }
 
     }
-
+    //for terminal input of item or products
     static ArrayList<String> getInputData(){
         ArrayList<String> inputList = new ArrayList<>();
         while (true) {
@@ -49,6 +63,7 @@ public class Main {
         return inputList;
     }
 
+    // Creating output data
     static ArrayList<String> getOutputData(ArrayList<String> inputList){
         ArrayList<String> outputList = new ArrayList<>();
         double totalTax = 0;
@@ -75,6 +90,53 @@ public class Main {
         return outputList;
     }
 
+    // for file input of product
+    static ArrayList<String> getFileInput() throws IOException{
+        Path path = Paths.get(inputFilename);
+        ArrayList<String> inputList = new ArrayList<>();
+
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String input;
+            while ((input = br.readLine()) != null) {
+                input = input.trim().replaceAll(" +", " ");
+                if(!checkInput(input)) {
+                    System.out.println("Your Didn't input product info according to the format\n");
+                    System.out.println("Enter items or product to calculate Sale Tax: \n" +
+                            "Example 1 book at 12.49\n" +
+                            "        1 music CD at 14.99\n" +
+                            "        1 chocolate bar at 0.85");
+                    System.exit(0);
+                }else{
+                    inputList.add(input);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return inputList;
+    }
+    
+    // for file output of product or items
+    static void getFileOutput(ArrayList<String> outputs) throws IOException {
+        Path path = Paths.get(outputFilename);
+        BufferedWriter bw = Files.newBufferedWriter(path);
+        try {
+            Iterator<String> item = outputs.iterator();
+            while (item.hasNext()) {
+                String line = item.next();
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (bw != null) {
+                bw.close();
+            }
+        }
+    }
+
+    // to check if line contains any specific item or product
     static  boolean containAny(String line, String[] list){
         for(String i : list){
             if(line.contains(i.toLowerCase())){
@@ -83,6 +145,7 @@ public class Main {
         }
         return false;
     }
+    // validating the input 
     static boolean checkInput(String input){
         if(!input.contains(" at ")){
             return false;
